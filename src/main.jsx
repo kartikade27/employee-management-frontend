719ad7950a004d1fd67e556e29d1d835a1d4163f
kartikade27 <kartikade27@gmail.com>
2026-03-08 11:39:28 +0530

    Updated main

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <BrowserRouter>
+    <HashRouter>
     
         <App />
-    </BrowserRouter>
+    </HashRouter>
   </AuthProvider>,
 );
